301e7949708b22d4a0ac3a29d34d0f9cde4341e0
pintu4146 <38511025+pintu4146@users.noreply.github.com>
2023-01-14 17:10:15 +0530

    Added solution - LeetHub

---
+++ Find the Frequency - GFG/find-the-frequency.py
@@ -0,0 +1,29 @@
+#User function Template for python3
+
+"""
+You're given an array (arr) of length n
+Return the frequency of element x in the given array
+"""
+def findFrequency (arr, n, x):
+    rtn=0
+    for i in arr:
+        if i==x:
+            rtn+=1
+    return rtn
+    # Your Code Here
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+t = int (input ())
+for tc in range (t):
+    n = int (input ())
+    arr = list (map (int, input ().split ()))
+    x = int (input ())
+    print (findFrequency (arr, n, x))
+    
+
+# } Driver Code Ends
